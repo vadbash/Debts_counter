1cca7614ba47a2134e5ce686fcc52e8be88c5b72
vadbash <vadusikvadim@gmail.com>
2025-04-18 15:01:37 +0200

    config

---
+++ config.py
@@ -0,0 +1,9 @@
+# Telegram bot connection
+telegram_bot_token = "Your_telegram_bot_token"
+
+# Database connection
+host="host"
+db_name = "db_name"
+db_username = "db_username"
+db_password = "db_password"
+auth_plugin='auth_plugin'
